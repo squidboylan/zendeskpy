cb31c52db653f2fcc9d24e90aa3729706dec111d
Caleb Boylan <caleb.boylan@dreamhost.com>
2016-06-11 15:29:59 -0700

    Add paging to article gets

--- zendeskpy/HelpCenter.py
+++ zendeskpy/HelpCenter.py
@@ -10,29 +10,41 @@ class HelpCenter(Base):
         self.email = email
         self.password = password
 
+    def _get_articles(self, url):
+        articles = self.get(url, self.email, self.password)
+        next_page_url = articles['next_page']
+
+        while next_page_url is not None:
+            next_page_json = self.get(next_page_url, self.email, self.password)
+            articles['articles'] = articles['articles'] + next_page_json['articles']
+            next_page_url = next_page_json['next_page']
+
+        articles['next_page'] = None
+        return articles
+
     def get_all_articles(self):
         url = domain + '/api/v2/help_center/articles.json'
-        return self.get(url)
+        return self._get_articles(url)
 
     def get_articles_by_locale(self, locale):
         url = domain + '/api/v2/help_center/{locale}/articles.json'.format(locale=locale)
-        return self.get(url)
+        return self._get_articles(url)
 
     def get_articles_by_category(self, category_id):
         url = domain + '/api/v2/help_center/categories/{id}/articles.json'.format(id=category_id)
-        return self.get(url)
+        return self._get_articles(url)
 
     def get_articles_by_section(self, section_id):
         url = domain + '/api/v2/help_center/sections/{id}/articles.json'.format(id=section_id)
-        return self.get(url)
+        return self._get_articles(url)
 
     def get_articles_by_user(self, user_id):
         url = domain + '/api/v2/help_center/users/{id}/articles.json'.format(id=user_id)
-        return self.get(url)
+        return self._get_articles(url)
 
     def get_articles_by_change(self, start_time)
         url = domain + '/api/v2/help_center/incremental/articles.json?start_time={start_time}'.format(start_time=start_time)
-        return self.get(url)
+        return self._get_articles(url)
 
 if __name__ == "__main__":
     domain = sys.argv[1]
