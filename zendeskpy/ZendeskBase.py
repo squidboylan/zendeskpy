58b8e4cc45d3abf16c35f4c23f15091c6adf3e2a
Caleb Boylan <caleb.boylan@dreamhost.com>
2016-06-11 14:39:09 -0700

    Add rate limit support

--- zendeskpy/ZendeskBase.py
+++ zendeskpy/ZendeskBase.py
@@ -2,26 +2,44 @@ from __future__ import print_function
 import requests
 import json
 import sys
+import time
 
 class Base:
     def get(self, url, email=None, password=None):
         session = requests.Session()
         session.auth = (email, password)
         response_raw = session.get(url)
-        response_json = json.loads(response_raw.content)
-        return response_json
+
+        if response_raw.status_code == 429:
+            time.sleep(response_raw.headers['Retry-After'])
+            return get(url, email, password)
+
+        else:
+            response_json = json.loads(response_raw.content)
+            return response_json
 
     def put(self, url, data, email=None, password=None):
         session = requests.Session()
         session.auth = (email, password)
         session.headers = {'Content-Type': 'application/json'}
-        r = session.put(url, data)
-        return r
+        response_raw = session.put(url, data)
+
+        if response_raw.status_code == 429:
+            time.sleep(response_raw.headers['Retry-After'])
+            return put(url, data, email, password)
+
+        else:
+            return response_raw
 
     def post(self, url, data, email=None, password=None):
         session = requests.Session()
         session.auth = (email, password)
         session.headers = {'Content-Type': 'application/json'}
-        r = session.post(url, data)
-        return r
+        response_raw = session.post(url, data)
 
+        if response_raw.status_code == 429:
+            time.sleep(response_raw.headers['Retry-After'])
+            return put(url, data, email, password)
+
+        else:
+            return response_raw
