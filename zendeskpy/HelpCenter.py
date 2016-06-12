be15402a6a7e8ab04656f4c55720aee46fa24e1e
Caleb Boylan <caleb.boylan@dreamhost.com>
2016-06-11 19:39:17 -0700

    Add more article functions

--- zendeskpy/HelpCenter.py
+++ zendeskpy/HelpCenter.py
@@ -23,28 +23,52 @@ class HelpCenter(Base):
         return data
 
     def get_all_articles(self):
-        url = domain + '/api/v2/help_center/articles.json?per_page=100'
+        url = self.domain + '/api/v2/help_center/articles.json?per_page=100'
         return self._page_gets(url, 'articles')
 
     def get_articles_by_locale(self, locale):
-        url = domain + '/api/v2/help_center/{locale}/articles.json?per_page=100'.format(locale=locale)
+        url = self.domain + '/api/v2/help_center/{locale}/articles.json?per_page=100'.format(locale=locale)
         return self._page_gets(url, 'articles')
 
     def get_articles_by_category(self, category_id):
-        url = domain + '/api/v2/help_center/categories/{id}/articles.json?per_page=100'.format(id=category_id)
+        url = self.domain + '/api/v2/help_center/categories/{id}/articles.json?per_page=100'.format(id=category_id)
         return self._page_gets(url, 'articles')
 
     def get_articles_by_section(self, section_id):
-        url = domain + '/api/v2/help_center/sections/{id}/articles.json?per_page=100'.format(id=section_id)
+        url = self.domain + '/api/v2/help_center/sections/{id}/articles.json?per_page=100'.format(id=section_id)
         return self._page_gets(url, 'articles')
 
     def get_articles_by_user(self, user_id):
-        url = domain + '/api/v2/help_center/users/{id}/articles.json?per_page=100'.format(id=user_id)
+        url = self.domain + '/api/v2/help_center/users/{id}/articles.json?per_page=100'.format(id=user_id)
         return self._page_gets(url, 'articles')
 
     def get_articles_by_change(self, start_time):
-        url = domain + '/api/v2/help_center/incremental/articles.json?start_time={start_time}?per_page=100'.format(start_time=start_time)
+        url = self.domain + '/api/v2/help_center/incremental/articles.json?start_time={start_time}?per_page=100'.format(start_time=start_time)
         return self._page_gets(url, 'articles')
+
+    def show_article(self, article_id, locale=None):
+        if not locale:
+            url = self.domain + '/api/v2/help_center/articles/{id}.json'.format(id=article_id)
+            return self.get(url, self.email, self.password)
+        else:
+            url = self.domain + '/api/v2/help_center/{locale}/articles/{id}.json'.format(id=article_id, locale=locale)
+            return self.get(url, self.email, self.password)
+
+    def create_article(self, section_id, data, locale=None):
+        if not locale:
+            url = self.domain + '/api/v2/help_center/sections/{id}/articles.json'.format(id=section_id)
+            return self.post(url, data, self.email, self.password)
+        else:
+            url = self.domain + '/api/v2/help_center/{locale}/sections/{id}/articles.json'.format(id=section_id, locale=locale)
+            return self.post(url, data, self.email, self.password)
+
+    def update_article_metadata(self, article_id, data, locale=None):
+        if not locale:
+            url = self.domain + '/api/v2/help_center/articles/{id}.json'.format(id=article_id)
+            return self.post(url, data, self.email, self.password)
+        else:
+            url = self.domain + '/api/v2/help_center/{locale}/articles/{id}.json'.format(id=article_id, locale=locale)
+            return self.post(url, data, self.email, self.password)
 
 if __name__ == "__main__":
     domain = sys.argv[1]
