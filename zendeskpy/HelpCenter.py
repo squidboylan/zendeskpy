084ba719187c8b533d8c804097b9edcba9c07c79
Caleb Boylan <caleb.boylan@dreamhost.com>
2016-06-11 18:56:36 -0700

    add combine key to paging function

--- zendeskpy/HelpCenter.py
+++ zendeskpy/HelpCenter.py
@@ -10,41 +10,41 @@ class HelpCenter(Base):
         self.email = email
         self.password = password
 
-    def _get_articles(self, url):
-        articles = self.get(url, self.email, self.password)
-        next_page_url = articles['next_page']
+    def _page_gets(self, url, combine_key):
+        data = self.get(url, self.email, self.password)
+        next_page_url = data['next_page']
 
         while next_page_url is not None:
             next_page_json = self.get(next_page_url, self.email, self.password)
-            articles['articles'] = articles['articles'] + next_page_json['articles']
+            data[combine_key] = data[combine_key] + next_page_json[combine_key]
             next_page_url = next_page_json['next_page']
 
-        articles['next_page'] = None
-        return articles
+        data['next_page'] = None
+        return data
 
     def get_all_articles(self):
         url = domain + '/api/v2/help_center/articles.json'
-        return self._get_articles(url)
+        return self._page_gets(url, 'articles')
 
     def get_articles_by_locale(self, locale):
         url = domain + '/api/v2/help_center/{locale}/articles.json'.format(locale=locale)
-        return self._get_articles(url)
+        return self._page_gets(url, 'articles')
 
     def get_articles_by_category(self, category_id):
         url = domain + '/api/v2/help_center/categories/{id}/articles.json'.format(id=category_id)
-        return self._get_articles(url)
+        return self._page_gets(url, 'articles')
 
     def get_articles_by_section(self, section_id):
         url = domain + '/api/v2/help_center/sections/{id}/articles.json'.format(id=section_id)
-        return self._get_articles(url)
+        return self._page_gets(url, 'articles')
 
     def get_articles_by_user(self, user_id):
         url = domain + '/api/v2/help_center/users/{id}/articles.json'.format(id=user_id)
-        return self._get_articles(url)
+        return self._page_gets(url, 'articles')
 
-    def get_articles_by_change(self, start_time)
+    def get_articles_by_change(self, start_time):
         url = domain + '/api/v2/help_center/incremental/articles.json?start_time={start_time}'.format(start_time=start_time)
-        return self._get_articles(url)
+        return self._page_gets(url, 'articles')
 
 if __name__ == "__main__":
     domain = sys.argv[1]
