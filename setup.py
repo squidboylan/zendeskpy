1e61b5869e5e6fd6690bb70271df87018bc06202
Caleb Boylan <caleb.boylan@dreamhost.com>
2016-06-25 22:03:33 -0700

    release 0.2.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setuptools.setup(
         description = 'Python module for interacting with the Zendesk Help Center API',
         author_email = 'calebboylan@gmail.com',
         url = 'https://github.com/squidboylan/zendeskpy',
-        version = '0.2.1',
+        version = '0.2.2',
         classifiers = [
             'Development Status :: 3 - Alpha',
             'Intended Audience :: Developers',
